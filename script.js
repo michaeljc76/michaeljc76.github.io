eaa0a71520cc9dcc20e70300258d0f2322f3f610
Michael Crowley <nico404.site@gmail.com>
2023-08-15 16:12:14 -0400

    Added old site materials

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#container").mousemove(function(e) {
+  parallaxIt(e, ".slide", -100);
+  parallaxIt(e, "img", -30);
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $("#container");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
