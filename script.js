6712f6c30cf7c1f6880a43ef27371fcdde5439e9
Michael Crowley <nico404.site@gmail.com>
2023-08-22 16:37:42 -0400

    Added a splash screen and lang=en to each page's html tag

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var splashScreen = document.querySelector('.splash');
+
+function hideSplashScreen() {
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  }, 610)
+}
+
+setTimeout(hideSplashScreen, 1000);
+
+/*
+splashScreen.addEventListener('click',()=>{
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  }, 610)
+})
+*/
+
+/*
 $("#container").mousemove(function(e) {
   parallaxIt(e, ".slide", -100);
   parallaxIt(e, "img", -30);
@@ -13,3 +34,4 @@ function parallaxIt(e, target, movement) {
     y: (relY - $this.height() / 2) / $this.height() * movement
   });
 }
+*/
