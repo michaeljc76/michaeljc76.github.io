c743b863e17ab1de6a73d5ff2a0c6b1c31b14170
Michael Crowley <nico404.site@gmail.com>
2023-09-22 18:02:51 -0400

    changed the importmap

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 import '/public/css.css'
-import * as THREE from '/node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls';
 
 /* THREE JS */
