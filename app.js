edd44c4b5726e54066e46fe3ff116505ba21dcd9
Michael Crowley <nico404.site@gmail.com>
2023-09-28 22:55:23 -0400

    test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+this.express.use('/public', express.static(publicPath));
