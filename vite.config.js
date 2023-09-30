f1c5d2a8e5dc89df6e7f2a22fc8a192c538e2ac7
Michael Crowley <nico404.site@gmail.com>
2023-09-30 00:01:46 -0400

    added other files to vite config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/public/'
-      : '/'
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        contact: './contact.html',
+        projects: './projects.html',
+        resume: './resume.html',
+        // ...
+        // List all files you want in your build
+      }
+    }
   }
+})
