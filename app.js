25f1edd7136cbbef7a080f1c67fa8c34f1fdaefa
Michael Crowley <nico404.site@gmail.com>
2023-10-02 13:35:23 -0400

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-// var path = require('path');
-// var app = express();
-
-// app.use(express.static(__dirname + 'public')); //Serves resources from public folder
-// this.express.use('/public', express.static(publicPath));
