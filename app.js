8ae5c1dba52bf6bdc0d1d0a2070af10345488f1d
Michael Crowley <nico404.site@gmail.com>
2023-10-02 13:05:40 -0400

    test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var app = express();
+// var path = require('path');
+// var app = express();
 
-app.use(express.static(__dirname + 'public')); //Serves resources from public folder
-this.express.use('/public', express.static(publicPath));
+// app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+// this.express.use('/public', express.static(publicPath));
