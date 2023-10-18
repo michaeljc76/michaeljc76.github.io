4747f01d963951a925466d766d678308497f976a
Michael Crowley <nico404.site@gmail.com>
2023-10-18 00:45:13 -0400

    test

--- globe.js
+++ globe.js
@@ -2,7 +2,6 @@ import '/css.css'
 import * as THREE from '/node_modules/three/build/three.module.js';
 import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import GLOBE from '/3Dmodels/wireframe_earth.glb'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -22,7 +21,7 @@ controls.enablePan = false;
 controls.enableZoom = false;
 
 let earth;
-loader.load( GLOBE, function ( glb ) {
+loader.load('models/wireframe_earth.glb', function ( glb ) {
     earth = glb;
 
     const mesh = glb.scene
