1f9deca68872ca85b773b99c81c9364b1e861e16
Michael Crowley <nico404.site@gmail.com>
2023-10-19 19:30:23 -0400

    Added computer model to swap with globe for projects tab

--- globe.js
+++ globe.js
@@ -4,6 +4,7 @@ import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitCont
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import gsap from 'gsap';
 import GLOBE from '/models/wireframe_earth.glb'
+import COMPUTER from '/models/computer.glb'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -27,46 +28,96 @@ controls.maxPolarAngle = Math.PI/2;
 let earth;
 loader.load( GLOBE , function ( glb ) {
     earth = glb;
-
     const mesh = glb.scene
     mesh.scale.set(15, 15, 15);
     mesh.position.set(0, 100, 0);
     scene.add( mesh );
 }, undefined, function ( error ) {
-
 	console.error( error );
-
 } );
 
- const ambientlight = new THREE.AmbientLight( 0xffffff, 0.2);
- //const light = new THREE.PointLight( 0xffffff, 10, 100)
- scene.add( ambientlight );
- //scene.add( light );
+let computer;
+loader.load( COMPUTER , function ( glb ) {
+    computer = glb;
+    const mesh = glb.scene
+    mesh.scale.set(3, 3, 3);
+    mesh.position.set(0, 100, 0);
+    //mesh.rotation.set(Math.PI / 10, -Math.PI / 6, 0)
+    scene.add( mesh );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
-// window.addEventListener('mousemove', onMouseMove);
+const blink = document.getElementById('blink');
+const videoTexture = new THREE.VideoTexture(blink);
+videoTexture.minFilter = THREE.LinearFilter;
+videoTexture.magFilter = THREE.LinearFilter;
+videoTexture.encoding = THREE.sRGBEncoding;
 
-// SHOW AND HIDE GLOBE SCRIPTS
+const videoMat = new THREE.MeshBasicMaterial({
+  map: videoTexture,
+  side: THREE.FrontSide,
+  toneMapped: false,
+});
 
-const hideButtons = document.getElementsByClassName('hideGlobe');
-for(var i = 0; i < hideButtons.length; i++){
-  hideButtons[i].addEventListener( 'click', hideGlobe, false );
-}
-//hideButton.addEventListener( 'click', hideGlobe, false );
+const videoGeo = new THREE.PlaneGeometry(27.5, 15.46875);
+const videoScreen = new THREE.Mesh(videoGeo, videoMat);
 
-const showButtons = document.getElementsByClassName('showGlobe');
-for(var i = 0; i < showButtons.length; i++){
-  showButtons[i].addEventListener( 'click', showGlobe, false );
-}
-//showButton.addEventListener( 'click', showGlobe, false );
+scene.add(videoScreen);
+videoScreen.position.set(0, 100, -0.6);
+
+const ambientlight = new THREE.AmbientLight( 0xffffff, 0.2);
+scene.add( ambientlight );
+
+// SCRIPTS TO SHOW AND HIDE GLOBE
+
+const homeButton = document.querySelector('.homebutton');
+const contactButton = document.querySelector('.contactbutton');
+const projectsButton = document.querySelector('.projectsbutton');
+const resumeButton = document.querySelector('.resumebutton');
+
+
+homeButton.addEventListener( 'click', () => {
+  hideGlobe();
+  hideComputer();
+}, false );
+
+contactButton.addEventListener( 'click', () => {
+  showGlobe();
+  hideComputer();
+}, false );
+
+projectsButton.addEventListener( 'click', () => {
+  hideGlobe();
+  showComputer();
+}, false );
+
+resumeButton.addEventListener( 'click', () => {
+  hideGlobe();
+  hideComputer();
+}, false );
 
 function showGlobe(){
   gsap.to(earth.scene.position, {y: 0, duration: 0.5});
-  //earth.scene.position.lerp(new THREE.Vector3(0, 0, 0), 0.1);
+  controls.reset();
 }
 
 function hideGlobe(){
   gsap.to(earth.scene.position, {y: 100, duration: 0.5});
-  //earth.scene.position.lerp(new THREE.Vector3(0, 100, 0), 0.1);
+}
+
+function showComputer(){
+  gsap.to(computer.scene.position, {y: -8, duration: 0.5});
+  controls.reset();
+  camera.rotation.set(-Math.PI/6, Math.PI/10, 0);
+  camera.position.set(17, 22, 40);
+  gsap.to(videoScreen.position, {y: 3.1, duration: 0.5});
+} 
+
+function hideComputer(){
+  gsap.to(computer.scene.position, {y: 100, duration: 0.5});
+  gsap.to(videoScreen.position, {y: 100, duration: 0.5});
+
 }
 
 function animate(){
--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,5 @@ export default defineConfig({
       },
     },
   },
-  assetsInclude: ['models/wireframe_earth.glb']
+  assetsInclude: ['models/wireframe_earth.glb', 'models/computer.glb']
 })
